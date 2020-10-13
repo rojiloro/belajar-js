4f2bcf71b50ee7c7ae7cc297927c736ba9156889
Oji <kelengoji79@gmail.com>
2020-10-13 14:19:03 +0700

    membuat pop up

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-var x = 10;
-console.log("hello world");
-console.log("isi dari variabel x adalah "+x);
+alert ("Selamat datang wahai user");
+var coba = true;
+
+while (coba){
+    var nama = prompt('Siapakah Anda');
+    alert ("wahai user yang bernama " + nama + " okaeri");
+    coba = confirm("apakah kamu akan mencoba inputan lagi??");
+}
+
+alert ("Terimakasih telah mampir");
