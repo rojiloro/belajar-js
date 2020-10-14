4c813ec7fdf3a875073af17cf7bacfc6332bdc56
Oji <kelengoji79@gmail.com>
2020-10-14 10:28:04 +0700

    perulangan for

---
+++ angkot2.js
@@ -0,0 +1,11 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+while (noAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. '+ noAngkot +' beroperasi dengan baik');
+    noAngkot++;
+}
+for (noAngkot= angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++){
+    console.log('Angkot No. '+ noAngkot +' tidak beroperasi dengan baik')
+}
---
+++ for.js
@@ -0,0 +1,3 @@
+for (var nama = 1; nama <= 10; nama++){
+    console.log('hai Oji.. '+nama);
+}
