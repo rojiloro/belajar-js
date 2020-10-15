ec1c94007a87f9a93e8bb98344e3d2b2a78e0be0
Oji <kelengoji79@gmail.com>
2020-10-15 12:38:52 +0700

    belajar menggunakan if bag 1

--- angkot2.js
+++ angkot2.js
@@ -2,10 +2,20 @@ var jmlAngkot = 10;
 var angkotBeroperasi = 6;
 var noAngkot = 1;
 
-while (noAngkot <= angkotBeroperasi) {
-    console.log('Angkot No. '+ noAngkot +' beroperasi dengan baik');
-    noAngkot++;
-}
-for (noAngkot= angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++){
-    console.log('Angkot No. '+ noAngkot +' tidak beroperasi dengan baik')
+// while (noAngkot <= angkotBeroperasi) {
+//     console.log('Angkot No. '+ noAngkot +' beroperasi dengan baik');
+//     noAngkot++;
+// }
+// for (noAngkot= angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++){
+//     console.log('Angkot No. '+ noAngkot +' tidak beroperasi dengan baik')
+// }
+
+if (jmlAngkot){
+    for(noAngkot <= angkotBeroperasi; noAngkot++){
+        console.log('Angkot No. '+ noAngkot +' beroperasi dengan baik');
+    }
+}else{
+    for(noAngkot = angkotBeroperasi; noAngkot <= jmlAngkot; noAngkot++){
+        console.log('Angkot No. '+ noAngkot +'tidak beroperasi dengan baik');
+    }
 }
