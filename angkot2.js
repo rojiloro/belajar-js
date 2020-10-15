c241bc85e7ecc518dd95af0d7ea54bbd6e6579fa
Oji <kelengoji79@gmail.com>
2020-10-15 14:51:28 +0700

    tentang if

--- angkot2.js
+++ angkot2.js
@@ -1,17 +1,11 @@
 var jmlAngkot = 10;
 var angkotBeroperasi = 6;
+var noAngkot = 1;
 
-// while (noAngkot <= angkotBeroperasi) {
-//     console.log('Angkot No. '+ noAngkot +' beroperasi dengan baik');
-//     noAngkot++;
-// }
-// for (noAngkot= angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++){
-//     console.log('Angkot No. '+ noAngkot +' tidak beroperasi dengan baik')
-// }
-for(var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++){
-    if (noAngkot <= angkotBeroperasi){
-            console.log('Angkot No. '+ noAngkot +' beroperasi dengan baik');
-    }else{
-            console.log('Angkot No. '+ noAngkot +' tidak beroperasi dengan baik');
-    }
+while (noAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. '+ noAngkot +' beroperasi dengan baik');
+    noAngkot++;
+}
+for (noAngkot= angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++){
+    console.log('Angkot No. '+ noAngkot +' tidak beroperasi dengan baik')
 }
--- script.js
+++ script.js
@@ -3,8 +3,17 @@ var coba = true;
 
 while (coba){
     var nama = prompt('Siapakah Anda');
-    alert ("wahai user yang bernama " + nama + " okaeri");
+    alert ("wahai user yang bernama " + nama + " ,okaeri");
     coba = confirm("apakah kamu akan mencoba inputan lagi??");
 }
 
 alert ("Terimakasih telah mampir");
+
+// var angka = prompt('Masukan Angka: ');
+// if(angka % 2 == 0){
+//     alert('Angka yang dimasukan adalah Genap');
+// } else if(angka % 2 == 1){
+//     alert('Angka yang dimasukan adalah Ganjil');
+// }else{
+//     alert('Anda tidak memasukan sebuah angka');
+// }
