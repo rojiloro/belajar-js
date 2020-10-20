56f90549af4e8a53243b8f291fab09dad22f8a56
Oji <kelengoji79@gmail.com>
2020-10-20 09:58:03 +0700

    perulangan bersarang

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-alert ("Selamat datang wahai user");
-var coba = true;
-
-while (coba){
-    var nama = prompt('Siapakah Anda');
-    alert ("wahai user yang bernama " + nama + " ,okaeri");
-    coba = confirm("apakah kamu akan mencoba inputan lagi??");
+var s = '';
+for(var i = 10; i > 0; i--){
+    for( var h = 10; h >= i; h--){
+        s += '*'
+    }
+    s += '\n';
 }
-
-alert ("Terimakasih telah mampir");
-
-// var angka = prompt('Masukan Angka: ');
-// if(angka % 2 == 0){
-//     alert('Angka yang dimasukan adalah Genap');
-// } else if(angka % 2 == 1){
-//     alert('Angka yang dimasukan adalah Ganjil');
-// }else{
-//     alert('Anda tidak memasukan sebuah angka');
-// }
+console.log(s);
