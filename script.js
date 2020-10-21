a45225e060c978541b6505c6d07d5c66effa8acc
Oji <kelengoji79@gmail.com>
2020-10-21 21:53:05 +0700

    perulangan bersarang for bag 1

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var s = '';
-for(var i = 10; i > 0; i--){
+for(var i = 1; i <= 10; i++){
     for( var h = 10; h >= i; h--){
-        s += '*'
+            s += '*';
     }
     s += '\n';
+    for( var b = 1; b <= i; b++){
+        s += ' ';
+    }    
 }
 console.log(s);
