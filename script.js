c5364ffb842c66027061042a1e585e2aef0c0a85
Oji <kelengoji79@gmail.com>
2020-10-23 11:00:39 +0700

    perulangan bersarang for bagian 2

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+var t = 20;
+var g = 10;
 var s = '';
-for(var i = 1; i <= 10; i++){
-    for( var h = 10; h >= i; h--){
-            s += '*';
+
+for(var i = 1; i <= t; i++){
+    if( i <= g ){
+        for( var h = 0; h < i; h++){
+                s += '*';
+        }
+    }else{
+        for( var y = 20; y > i; y--){
+                s += '*';
+        }
     }
-    s += '\n';
-    for( var b = 1; b <= i; b++){
-        s += ' ';
-    }    
+s += '\n';
 }
 console.log(s);
