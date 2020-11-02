b660b64d2352a3d17f4fee5052bfde7c3801a4e1
Oji <kelengoji79@gmail.com>
2020-11-02 14:14:46 +0700

    function 1

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-var x = new Array();
-var input = prompt('Masukan nilai x = ');
+// menghitung 2 buah volume kubus
+function jumlahVolumeDuaKubus(a, b){
+    var total, v1, v2;
+    
+    v1 = a * a * a;
+    v2 = b * b * b;
 
-for( i = 1; i <= input; i++){
-    x[i] = new Array();
-    for( j = 1; j <= i; j++){
-        if( j == 1 || j == i){
-            x[i][j] = 1;
-        }
-        else{
-            x[i][j] = x[i-1]+x[i-1][j];
-        }
-    }
+    total = v1 + v2;
+    return total;
 }
-for ( i = 1; i <= input; i++){
-    /*for( g = input; g > input; g--){
-        document.write(" ");
-    }*/
-    for( j = 1; j <= i; j++){
-        document.write(x[i][j]+'');
-    }
-    document.write("<br>");
-}
-//pola segitiga pascal adalah segitiga yang bentuknya
-// setiap baris diawali dengan 1. dan angka ditengah
-// akan ditambahkan dengan angka yang di sebelah kirinya
+
+alert(jumlahVolumeDuaKubus(8, 3));
+alert(jumlahVolumeDuaKubus(5, 2));
