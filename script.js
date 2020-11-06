f0f93e763a8b38cfcdd056e2008c0f5c81dacf3a
Oji <kelengoji79@gmail.com>
2020-11-06 10:57:22 +0700

    manipulasi array

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-var hari = [];
-hari = ['ahad','senin','selasa','rabu','kamis','jumat','sabtu'];
+// manipulasi array
 
-console.log(hari.length);
+// 1. menambah isi array
+
+// var arr = [];
+// arr[0] = "mohammad";
+// arr[1] = "Fatkhuroji";
+// arr[2] = "oji";
+// arr[4] = "keren";
+
+// console.log(arr);
+
+// 2. menghapus isi array
+// var arr = ['mohammad', 'fatkhuroji','ganteng'];
+// arr[1] = undefined;
+// console.log(arr);
+
+// 3. menampilkan array
+// var arr = ['Mohammad', 'Fatkhuroji','Ganteng'];
+
+// for(var i = 0; i < arr.length; i++){
+//     console.log('Mahasiswa ke- '+(i+1)+ ': ' +arr[i]);
+// }
+
+// method pada array
+// 1. join
+// var arr = ['mohammad', 'fatkhuroji','ganteng'];
+// console.log(arr.join(' - '));
+
+// 2 push dan pop
+var arr = ['mohammad', 'fatkhuroji','ganteng'];
+// arr.pop('joss');
+arr.shift();
+// arr.pop();
+// arr.pop();
+console.log(arr.join(' - '));
